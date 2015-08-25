3fa5b03dfc3b12b359def3a7e24a80d0bb807f4b
Jordan Navratil & Jenna Schold Navratil <student@epicodus.com>
2015-08-25 12:32:15 -0700

    add tests, all tests passing except for StudentTest GetAll.

--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -92,8 +92,30 @@
             $this->assertEquals($test_student, $result[0]);
         }
 
+        function test_GetAll()
+        {
+            $name = "Coding 101";
+            $enrollment_date = '0000-00-00';
+            $id = null;
+            $test_course = new Course($name, $id);
+            $test_course->save();
+
+            $student_name = "Aladdin";
+            $course_id = $test_course->getId();
+            $test_student = new Student ($student_name, $id, $enrollment_date, $course_id);
+            $test_student->save();
+
+            $student_name2 = "Jasmine";
+            $id2= null;
+            $course_id2 = $test_course->getId();
+            $test_student2 = new Student ($student_name2, $id2, $enrollment_date, $course_id2);
+            $test_student->save();
+
+            $result = Student::getAll();
+
+            $this->assertEquals(['test_student, $test_student2'], $result);
+        }
+
     }
-
-
 
 ?>
